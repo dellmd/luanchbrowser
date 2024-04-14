30a1d32c8bb1a5f9af5e929c4a07104ac277a8b4
dellmd <mdsgyanpur@gmail.com>
2024-04-14 07:17:15 +0530

    first create a project lunchbroser

---
+++ lunchbrowser/src/main/java/lunchbrowser/SimpleCode.java
@@ -0,0 +1,12 @@
+package lunchbrowser;
+
+public class SimpleCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "\\driver\\chromedriver.exe");
+		 
+
+	}
+
+}
